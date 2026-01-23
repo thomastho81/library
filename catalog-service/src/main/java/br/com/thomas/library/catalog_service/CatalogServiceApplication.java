4a903ef7b3f36a05558f50945a6b44f8ab20f07a
thomastho81 <thom.bp@hotmail.com>
2026-01-22 21:44:26 -0300

    feat: criacao modulo de catalogo

---
+++ catalog-service/src/main/java/br/com/thomas/library/catalog_service/CatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.thomas.library.catalog_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogServiceApplication.class, args);
+	}
+
+}
---
+++ catalog-service/src/test/java/br/com/thomas/library/catalog_service/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thomas.library.catalog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
